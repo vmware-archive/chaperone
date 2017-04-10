a9475c65661da50efbf3afbef229e821c41698b8
Tom Scanlan <tscanlan@vmware.com>
2017-04-10 09:43:05 -0400

    use correct orgurl
    Change-Id: Id12fc80d0dd287cfda6a9bd327c30709cf6a8356

--- repo-to-markdown.py
+++ repo-to-markdown.py
@@ -1,17 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+from __future__ import print_function
 
 import xmltodict
 import sys
 
 
-with open(sys.argv[1]) as fd:
-    doc = xmltodict.parse(fd.read())
 
-    vmwareorg = doc['manifest']['remote']['@fetch']
+for arg in sys.argv[1:]:
+    with open(arg) as fd:
+        doc = xmltodict.parse(fd.read())
 
-    print(vmwareorg)
-    for project in doc['manifest']['project']:
-        project
-        if project['@remote'] == 'vmwareorg' and '@name' in project:
-            print('  - [%s](%s)' % (project['@name'], vmwareorg + '/' + project['@name']))
+        orgname = doc['manifest']['remote']['@name']
+        orgurl = doc['manifest']['remote']['@fetch']
+
+        #print(vmwareorg)
+        for project in doc['manifest']['project']:
+            if '@name' in project:
+                if '/' in project['@name']:
+                    print('repo:%s' % (project['@name']), end=' ')
+                else:
+                    print('repo:%s/%s' % (orgname, project['@name']), end=' ')
+
+                #print('  - [%s](%s)' % (project['@name'], orgurl + '/' + project['@name']))
 
